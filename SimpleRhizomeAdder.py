38bb7a4c183c9b1ae4ad2c114d9a98d8d77d0847
Artur <github@artursterz.de>
2016-09-20 09:36:52 +0200

    Decreased noise.

--- SimpleRhizomeAdder.py
+++ SimpleRhizomeAdder.py
@@ -10,6 +10,8 @@ if __name__ == "__main__":
     count = 0
     while True:
     	rhizomeRandomFile(basename+"-"+str(count)+".bin", random.randint(1024, 4096))
+        print "inserting file"
         count += 1
-        insertion_delay_ms = 10000 + random.randint(0, 10000)
-        time.sleep(float(insertion_delay_ms)/1000)
+        insertion_delay = 20 + random.randint(0, 10)
+        print "sleeping {}".format(insertion_delay)
+        time.sleep(insertion_delay)
