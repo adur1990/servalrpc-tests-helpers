bf34ae46dd20a985ffe012a13a5adda7fe5ae0c6
Artur <github@artursterz.de>
2016-09-27 06:47:20 +0000

    Decreased noise file sizes

--- SimpleRhizomeAdder.py
+++ SimpleRhizomeAdder.py
@@ -6,12 +6,12 @@ from Helpers import *
 basename = socket.gethostname()
 random.seed(basename)
 
+data_set = [53, 512, 2048]
+
 if __name__ == "__main__":
     count = 0
     while True:
-    	rhizomeRandomFile(basename+"-"+str(count)+".bin", random.randint(1024, 4096))
-        print "inserting file"
+        rhizomeRandomFile(basename+"-"+str(count)+".bin", random.choice(data_set))
         count += 1
-        insertion_delay = 20 + random.randint(0, 10)
-        print "sleeping {}".format(insertion_delay)
+        insertion_delay = 60 + random.randint(0, 60)
         time.sleep(insertion_delay)
